8f82f0dcd73f782170f29a49626213b539700ae5
Pavel Kulyov <kulyov.pavel@gmail.com>
2016-06-21 19:19:00 +0300

    Declarative widget

--- kisqpy/client.py
+++ kisqpy/client.py
@@ -1,7 +1,5 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
 
 
 class LoginScreen(GridLayout):
@@ -9,14 +7,7 @@ class LoginScreen(GridLayout):
 
     def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(**kwargs)
-
         self.cols = 2
-        self.add_widget(Label(text="Username"))
-        self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
-        self.add_widget(Label(text="password"))
-        self.password = TextInput(password=True, multiline=False)
-        self.add_widget(self.password)
 
 
 class ClientApp(App):
